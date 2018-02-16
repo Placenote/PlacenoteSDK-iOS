581bffd6083f91acd1ed0e34cfcf5578bf3929c6
Brad Hoekstra <bhoekstra@perceptivlabs.com>
2018-02-16 14:20:12 -0500

    Make API key clearer

--- PlacenoteSDKExample/AppDelegate.swift
+++ PlacenoteSDKExample/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
-        LibPlacenote.instance.initialize(apiKey: API_KEY)
+        LibPlacenote.instance.initialize(apiKey: Enter API KEY here)
 
         return true
     }
