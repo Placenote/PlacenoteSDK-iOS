aa0e55ddb94f5f208fb4f9595b680be1e3d4969a
Yan Ma <yanma@Yans-MacBook-Pro.local>
2018-06-08 18:16:57 -0400

    fixed pod ruined by last commit

--- Pods/Target Support Files/Pods-PlacenoteSDKExample/Pods-PlacenoteSDKExample-frameworks.sh
+++ Pods/Target Support Files/Pods-PlacenoteSDKExample/Pods-PlacenoteSDKExample-frameworks.sh
@@ -143,11 +143,9 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/PlacenoteSDK/Placenote.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PlacenoteSDK/PlacenoteSDK.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/PlacenoteSDK/Placenote.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PlacenoteSDK/PlacenoteSDK.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
