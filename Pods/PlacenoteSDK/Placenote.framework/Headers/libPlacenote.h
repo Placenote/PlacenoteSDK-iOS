35486ad54fe390db3a54301d82bd4efb9fb72eb6
Prasenjit Mukherjee <prashenjitmuk@gmail.com>
2018-07-22 12:17:03 -0400

    updated pod to exit properly

--- Pods/PlacenoteSDK/Placenote.framework/Headers/libPlacenote.h
+++ Pods/PlacenoteSDK/Placenote.framework/Headers/libPlacenote.h
@@ -103,6 +103,7 @@ typedef void (*result_callback) (PNCallbackResult* result, void* context);
 typedef void (*transfer_map_callback) (PNTransferStatus* status, void* context);
 typedef void (*pose_callback) (PNTransform* placenotePose, PNTransform* arkitPose, void* context);
 
+
 FOUNDATION_EXPORT int PNInitialize(PNInitParams* params, result_callback cb, void* context);
 FOUNDATION_EXPORT int PNGetStatus ();
 FOUNDATION_EXPORT int PNGetTrackedLandmarks (PNFeaturePoint* points, int size);
@@ -113,7 +114,7 @@ FOUNDATION_EXPORT int PNSaveMap (const char* mapId, transfer_map_callback cb, vo
 FOUNDATION_EXPORT int PNLoadMap (const char* mapId, transfer_map_callback cb, void* context);
 FOUNDATION_EXPORT int PNDeleteMap (const char* mapId, result_callback cb, void* context);
 FOUNDATION_EXPORT int PNAddMap (result_callback cb, void* context);
-FOUNDATION_EXPORT int PNStartSession (pose_callback cb, bool extending, void* context);
+FOUNDATION_EXPORT int PNStartSession (pose_callback cb, void* context);
 FOUNDATION_EXPORT int PNStopSession ();
 FOUNDATION_EXPORT int PNStartRecordDataset(transfer_map_callback cb, void* context);
 FOUNDATION_EXPORT int PNGetMetadata (const char* mapId, result_callback cb, void* context);
@@ -121,5 +122,7 @@ FOUNDATION_EXPORT int PNSetMetadata (const char* mapId, const char* metadataJson
 FOUNDATION_EXPORT int PNGetPose(PNTransform* transform);
 FOUNDATION_EXPORT int PNSetIntrinsics(PNCameraInstrinsics* instrinsics);
 FOUNDATION_EXPORT int PNSetFrame(PNImagePlane* yPlane, PNImagePlane* vuPlane, PNTransform* arPose);
+FOUNDATION_EXPORT int PNShutdown();
+
 
 #endif  // LIBPLACENOTE_H
